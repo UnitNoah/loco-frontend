37f6b723f605c292be3d8478067e767456e66568
jinsung6543 <jinsung6543@gmail.com>
2025-08-08 22:41:19 -0700

    feat: implement login modal

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,13 +1,19 @@
 import { Outlet } from 'react-router-dom'
+import { useState } from 'react'
 import Header from './Header'
+import Login from '../Login'
 
 const Layout = () => {
+  const [isLoginOpen, setIsLoginOpen] = useState(false)
+
   return (
     <div className="min-h-screen">
-      <Header />
+      <Header onLoginClick={() => setIsLoginOpen(true)} />
       <main className="p-4">
         <Outlet />
       </main>
+
+      <Login open={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
     </div>
   )
 }
